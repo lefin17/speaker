63cc0026793090e748bb888e374210b997662d1d
Alexey Poliansky <lefin17@gmail.com>
2019-09-02 00:14:52 +0300

    some changes with config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+define("_PREFIX_", "sr_");
+// define("_DICT_", "dictionary_rus");
--- tests/test_az2.php
+++ tests/test_az2.php
@@ -14,15 +14,16 @@ for($i = 0; $i<iconv_strlen($alfabet); $i++)
     if (in_array($letter, $in)) { 
 	 // print "s";
 	 continue; }  
-    $q = "SELECT `word` FROM `sr_dictionary_rus` WHERE `word` like '%".
-	    $letter."%' ORDER BY RAND() LIMIT 1";
+    $q = "SELECT `example` FROM `sr_dictionary_rus` WHERE 
+	`word` like '%".$letter."%' and `example` != '' ORDER BY RAND() LIMIT 1";
 //	    print $q."\n";
+
     $r = mysqli_query($conn, $q);
     if (mysqli_num_rows($r) == 0) { print "ALERT EMPTY ".$letter."\n"; continue; } 
     
     list($word) = mysqli_fetch_row($r);
 //    print $word."\n";
-    $text .= (!empty($text) ? " " : "").$word; 
+    $text .= (!empty($text) ? "\n" : "").trim($word); 
     for($j = 0; $j < iconv_strlen($word); $j++)
 	{
 	$tmp = iconv_substr($word, $j, 1);
@@ -35,7 +36,7 @@ print $text."\n";
 $q = "INSERT INTO `sr_samples` 
 	(`sample_text`, 
          `created_on`,
-         `type`) VALUES ('".$text."', NOW(), 'test_az')";
+         `type`) VALUES ('".$text."', NOW(), 'test_az2')";
          
 //         print $q."\n";
 $r = mysqli_query($conn, $q);
