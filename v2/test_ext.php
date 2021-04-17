2d5047ce7817d9639162a1b273c20b380b242cac
Alexey Poliansky <lefin17@gmail.com>
2021-04-18 01:03:02 +0300

    saple rate fixed

--- v2/test_ext.php
+++ v2/test_ext.php
@@ -20,6 +20,43 @@ $end = 2 * 1000;  // to 2 seconds
 try {
     $ext = new AuderoWavExtractor($inputFile);
     $wav = $ext->getChunk($start, $end); //берем кусок данных из файла
+    
+    print_r(get_class_methods(get_class($ext)));
+    print "Data Rate: ".$ext->getWav()->getDataRate();
+//    print "SampleRate: ".$ext->getWav()->getSampleRate();
+    $w = $ext->getWav();
+    print_r(get_class_methods(get_class($w)));
+    $wc = $w->getSampleRate(); // Headers()[Chunk\Fmt::ID]->getSampleRate()->getValue();
+    
+    print $wc;
+    die();
+    // print_r($wc);
+    // ::getSampleRate();
+    
+    foreach($wc as $key => $obj)
+	{
+	print $key."\n";
+	print get_class($obj);
+	print_r(get_class_methods(get_class($obj)));
+	}
+//    print_r($wav);
+    
+    $h = $w->getHeaders();
+    foreach($h as $o)
+	{
+	//print get_class($o);
+	if (in_array("getSampleRate", get_class_methods(get_class($o))))
+	    {
+	    $K = $o->getSampleRate();
+	    print_r($K);
+	//    foreach($K as $key => $value)
+	//	 print "\n".$key." = ".$value."\n";
+		 }
+	}
+//	$h->
+//    print_r(get_class_methods(get_class($ext->getWav())));
+    
+//    print_r(get_class_methods(get_class($ext->getWav()->getHeaders())));
 //    $ext->saveChunk($start, $end, $outputFile);
 }
 catch (Exception $e) {
@@ -32,11 +69,11 @@ catch (Exception $e) {
 	 exit();
 	} //тип определяется на осное размерности данных
     //разбиение на фреймы
-    $length = 256; //длина фрейма  //кусок данных дополниться нулями до фрейма нужной длины.
+    $length = 256; //длина фрейма  //кусок данных дополниться нулями до фрейма нужной длины. - по хорошему перейти к временной шкале... 
     $f = array_chunk($data, 256);
     $i = count($f) - 1;
     $f[$i] = array_pad($f[$i], $length, 0);
-    print_r(end($f));
+
     //тут можно включать кластеризацию
     //
     //
--- v2/wav2e.php
+++ v2/wav2e.php
@@ -26,7 +26,7 @@ $inputFile = "./samples/aa_converted.wav";
 $inputFile = "./samples/aa_converted.wav";
 $outputFile = "./excerpt.wav";
 $start = 0 * 1000; // from 0 seconds
-$end = 2 * 1000;  // to 2 seconds
+$end = 11 * 1000;  // to 2 seconds
 
 //f - frame
 //fr - frame result as properties
@@ -36,8 +36,8 @@ try {
     $wav = $ext->getChunk($start, $end); //берем кусок данных из файла
 //    $ext->saveChunk($start, $end, $outputFile);
 }
-catch (Exception $e) {
-    echo "An error has occurred: " . $e->getMessage();
+catch (Exception $error) {
+    echo "An error has occurred: " . $error->getMessage();
     exit();
 }
 
@@ -46,28 +46,68 @@ catch (Exception $e) {
 	 exit();
 	} //тип определяется на осное размерности данных
     //разбиение на фреймы
-    $length = 256; //длина фрейма  //кусок данных дополниться нулями до фрейма нужной длины. - по хорошему перейти к временной шкале... 
+    $length = 256; //длина фрейма  
+		   //кусок данных дополниться нулями до фрейма нужной длины. - по хорошему перейти к временной шкале... 
+    
     $f = array_chunk($data, 256);
     $index = count($f) - 1;
     $f[$index] = array_pad($f[$index], $length, 0);
     for($i = 0; $i < count($f); $i++)
 	$e[$i] = array(energy($f[$i]));
-    $k = new KMeans(3);
     
-    $res = $k->cluster($e);
+    $k = new KMeans(5);
+    $res = $k->cluster($e); //пауза будет около 350мс для разреза файла
+    $minFrame = 0;
+    
     foreach($res as $clu => $arr)
-	foreach($arr as $index => $e)
+	foreach($arr as $index => $en)
 	{
 	$fr[$index]["energyCluster"] = $clu;
-	$fr[$index]["energy"] = $e;
+	$fr[$index]["energy"] = $en[0];
+	if (!isset($cutCluster))
+	    {
+	    $cutCluster = $clu;
+	    $cutEnergy = $en[0];
+	    }
+	    else 
+	    if ($cutEnergy > $en[0]) { $cutCluster = $clu; $cutEnergy = $en[0]; $minFrame = $index; } 
 	}
-	
+    $cutLength = 10; //if pause more then this -> it's cut time (с текущим sample rate - около 0.3 сек
+    $liveLength = 10; //if live length > 10 it's enouht to remember
+    $dieLength = 5; //will disapear not borned
+    $born = false;  	
+    $mass = 0; 
     for($i = 0; $i < count($f); $i++)
 	{
-	print $fr[$i]["energyCluster"];
+	$c = $fr[$i]["energyCluster"];
+	if ($c != $cutCluster)
+	    { $start_fr = $i; 
+	      $born = true; 
+	      $mass = 0;
+	      $cutEvent = 0; }  // 
+	
+	if ($born && $c == $cutCluster)
+	    { 
+	      if ($mass < $liveLength) $mass -= 1;
+	      if ($mass < -($dieLength)) $born = false; 
+	      $cutEvent++; 
+	     }
+	    
+	if ($born && $cutEvent >= $cutLength)
+	    {
+	    $start_fr = ($start_fr > 10) ? $start_fr : 0; //перевести в милисекунды если не ноль, добавить 0.2с 
+	    $end_fr = $i;
+	    //теперь сохранить из исходника файл с указанием номеров фреймов или лучше милисекунд
+	    
+	    }  
+	    
+	    
 	}
-    print_r($res);
-    
+//     print_r($res);
+
+    print "\nMIN ENERGY FRAME: ".$cutEnergy."\n";
+    print "CUT CLUSTER: ".$cutCluster."\n";
+    print "MIN FRAME: ".$minFrame."\n";
     //тут можно включать кластеризацию
     //
     //
